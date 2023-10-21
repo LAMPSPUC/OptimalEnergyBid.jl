9d9c57aa00a2526841fa0417700a1e3b05cf0f48
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2023-10-21 19:56:11 +0200

    fix stageobjective

--- src/objective.jl
+++ src/objective.jl
@@ -9,7 +9,7 @@ function set_real_time_commit_objective!(sp, prb::Problem, t::Int, k::Int)
 end
 
 function set_day_ahead_commit_objective!(sp, prb::Problem, t::Int, k::Int)
-    temp = div(t, prb.numbers.N) + 1
+    temp = div(t-1, prb.numbers.N)+1
     @stageobjective(sp, -sum(prb.random_variables.πᵧ[k,i,temp,n]*(sp[:day_ahead_commit][i,n+prb.numbers.N-prb.numbers.V].out) for i in 1:prb.numbers.I, n in 1:prb.numbers.N))
     return nothing
 end
--- test/test_train_model.jl
+++ test/test_train_model.jl
@@ -7,28 +7,28 @@ random = prb.random_variables
 data = prb.data
 options = prb.options
 
-values_beta = zeros(2,1,4)
-values_gamma = zeros(2,1,2,2)
-probabilities = zeros(2,4)
-probabilities[:,:] .= 0.5
+values_beta = zeros(1,1,1)
+values_gamma = zeros(1,1,1,1)
+probabilities = zeros(1,1)
+probabilities[:,:] .= 1.0
 values_beta[:,:,:] .= 10.0
 values_gamma[:,:,:,:] .= 9.0
 random.πᵦ = values_beta
 random.ωᵦ = probabilities
 random.πᵧ = values_gamma
 random.ωᵧ = probabilities
-numbers.N = 2
-numbers.n₀ = 2
+numbers.N = 1
+numbers.n₀ = 1
 numbers.I = 1
-numbers.T = 4
-numbers.Kᵦ = 2
-numbers.Kᵧ = 2
+numbers.T = 1
+numbers.Kᵦ = 1
+numbers.Kᵧ = 1
 numbers.U = 1
-numbers.V = 2
+numbers.V = 1
 
-data.V_max = ones(2)
-data.V_min = zeros(2)
-data.V_0 = ones(2)
+data.V_max = ones(1)
+data.V_min = zeros(1)
+data.V_0 = ones(1)
 
 options.optimizer = HiGHS.Optimizer
 
