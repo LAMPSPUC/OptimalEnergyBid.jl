bb2dc8ea12461e07cd4e8130c13d6996968f2742
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2023-10-29 21:23:06 +0100

    test

--- examples/deterministic.jl
+++ examples/deterministic.jl
@@ -5,3 +5,12 @@ prb.options.optimizer = HiGHS.Optimizer
 build_model!(prb)
 train!(prb)
 simul = simulate!(prb, 2)
+
+prb.output.day_ahead_bid
+prb.output.day_ahead_clear
+prb.output.real_time_bid
+prb.output.generation
+
+for i = 1:18
+    @show simul[1][i][:day_ahead_clear]
+end
