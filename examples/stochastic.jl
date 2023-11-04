097e52a6de1b8e10ec1f52e04e692105e3d9a575
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2023-11-04 20:36:01 +0100

    rename

--- examples/stochastic.jl
+++ examples/stochastic.jl
@@ -1,7 +1,20 @@
 using MaxStorageRevenue, HiGHS
 
-prb = create_problem(joinpath(dirname(dirname(@__FILE__)), "cases", "stochastic_case.json"))
+prb = create_problem(joinpath(dirname(dirname(@__FILE__)), "cases", "stochastic.json"))
 set_optimizer!(prb, HiGHS.Optimizer)
 build_model!(prb)
-train!(prb)
+train!(prb, time_limit=10)
 simul = simulate!(prb, 2)
+
+plot_all(prb, 2, "")
+
+prb.random_variables.ωᵪ = [
+    0.5  0.5  0.5  0.5  0.5  0.5
+    0.5  0.5  0.5  0.5  0.5  0.5
+]
+
+build_model!(prb)
+train!(prb, time_limit=10)
+simul = simulate!(prb, 2)
+
+plot_all(prb, 2, "")
