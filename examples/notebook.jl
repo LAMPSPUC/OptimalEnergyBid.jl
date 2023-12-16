e4934aa88e4404474c7cda5d05c40aa11410b038
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2023-12-16 15:24:52 +0100

    finish notebook

--- examples/notebook.jl
+++ examples/notebook.jl
@@ -322,8 +322,15 @@ load("generation.png")
 md"""
 # Risk aversion case
 ## Over time, the best real-time price gets much better and the worst real-time price gets a little worse.
-- real-time price is 1.5 - 0.1t and 2.0 + 2.5t (50% each one)
+- 2 units
+- 3 days
+- 2 jours per day
+- min storage is 0
 - max storage is 100
+- initial storage is 0.0 and 0.5
+- real-time price is 1.5 - 0.1t and 2.0 + 2.5t (50% each one)
+- day-ahead price is 1.0 and 1.4 (50% each one)
+- inflow is 0.5 per unit
 """
 
 # ╔═╡ 3d647090-4800-471f-ad98-074d8556e19b
@@ -366,24 +373,24 @@ end
 load("volume.png")
 
 # ╔═╡ Cell order:
-# ╠═94c16ad4-7dbf-49cb-beda-f301ac7d8e53
-# ╠═af7a6e30-9723-11ee-18f6-2552c59ac981
+# ╟─94c16ad4-7dbf-49cb-beda-f301ac7d8e53
+# ╟─af7a6e30-9723-11ee-18f6-2552c59ac981
 # ╟─6161e8fc-ccf8-47bb-819a-6fae07c008bb
-# ╠═71ec49f5-5b71-43e5-8e8a-e7f4e369618a
+# ╟─71ec49f5-5b71-43e5-8e8a-e7f4e369618a
 # ╟─f7528f2f-dd8a-4a80-be11-30ca505738bc
 # ╟─84a93406-66af-4d11-bf04-fbbbbb8fa3d6
 # ╟─c7534d90-1de7-44ec-b3d1-2e0fc26ff3ac
 # ╟─84f7962e-00b2-443c-b218-7d4e3f8f55b4
 # ╟─9aae9053-7af3-44d7-bf27-76b14a0db7f5
 # ╟─e8b1ff03-9a79-4c28-8037-f0ab351468b7
-# ╠═2793fc42-c43f-4b44-a4cd-f9491e399168
+# ╟─2793fc42-c43f-4b44-a4cd-f9491e399168
 # ╟─dcb25596-8528-4c4c-ac68-2a6560e96ff2
 # ╟─c4bb7c7c-856d-4ac5-827c-a35e19d57da1
 # ╟─d46ee75b-494a-47e8-a89e-8c78d108e88c
 # ╟─8391884a-f5e9-4f74-a6d6-231633b50d83
 # ╟─d2387568-6ace-4ee9-b9fb-79e711f69d37
 # ╟─b3f6833c-b9c7-490c-8785-173407261b54
-# ╠═5972566b-58c1-45ba-bd3b-0353516b4cb4
+# ╟─5972566b-58c1-45ba-bd3b-0353516b4cb4
 # ╟─698f4b95-852c-40cd-b125-8d1426b5c6f2
 # ╟─9578f57b-7531-406b-8078-2bff6fd549c2
 # ╟─7c6cf9f4-4828-4238-ab2c-b87f5488e2af
@@ -391,14 +398,14 @@ load("volume.png")
 # ╟─1502015b-5663-47d4-99f8-ebae1b347ec1
 # ╟─43cdaf9c-754c-4ea2-986e-16ea22cd7662
 # ╟─f330b280-f7dd-4e1e-b50f-b08b4beb6952
-# ╠═f37e8a4f-512e-4cc6-bd2c-355c4c5612c8
+# ╟─f37e8a4f-512e-4cc6-bd2c-355c4c5612c8
 # ╟─213a0337-a34b-4af3-aaa7-4cdd99ac691a
 # ╟─59b650ab-4779-485a-b4a9-979bd7383376
 # ╟─76712d9a-62c8-4907-8186-6cd51a22c50a
 # ╟─fc6b76fb-b565-453f-8da4-6256a056e05b
 # ╟─267d6a5c-1ceb-459d-87e5-08dd5ea38c11
 # ╟─c5dc745c-ade3-4ce0-b8a8-ee90786f10dc
-# ╠═5c5a126b-bcea-4dfe-961d-b98b6ffb391a
+# ╟─5c5a126b-bcea-4dfe-961d-b98b6ffb391a
 # ╟─e3c8bb14-60f5-460e-8e45-9073807d5501
 # ╟─688f508f-9579-4c55-80bd-8ad4205bbb65
 # ╟─c583364c-0fc8-4ce7-aca8-f9cb198ba3ab
@@ -416,20 +423,20 @@ load("volume.png")
 # ╟─9b86efb4-ae0a-430c-9fc9-b2965cf60998
 # ╟─bc96ce51-f393-40ed-b55e-bae217032c05
 # ╟─2242fdbe-77f5-4d75-abc8-a21f7adfc324
-# ╠═31e13286-b414-464d-983a-b1dbd3a39ebf
+# ╟─31e13286-b414-464d-983a-b1dbd3a39ebf
 # ╟─f92891c9-85cb-4308-b948-a586018276e6
 # ╟─0ed061e0-7ad2-4cce-93b8-875c9c68cd29
 # ╟─0c90567a-78b3-45df-a4a6-c9928fdc0349
 # ╟─5e024476-ef59-4b4c-bf88-ae8ae52183a5
 # ╟─282729ad-6faa-498b-8a3e-f6b461080a8c
 # ╟─767e0d0f-552b-4761-a36b-0d3d9f0c9739
-# ╠═55de0841-c4c5-409c-beed-6a6bfb7eca39
+# ╟─55de0841-c4c5-409c-beed-6a6bfb7eca39
 # ╟─8c198f51-102d-4479-b956-11a3d4481df5
 # ╟─d838fc76-b123-472d-9e75-df903b5d3eae
 # ╟─b3e78c7a-be71-4a38-98c6-ac7e2aecfaae
 # ╟─d0a31085-ab02-4d4d-a5c1-f95c67c0e502
-# ╠═3d647090-4800-471f-ad98-074d8556e19b
+# ╟─3d647090-4800-471f-ad98-074d8556e19b
 # ╟─d5ab3d27-6c4a-4f49-a5cc-17d5cbcefc90
 # ╟─9e6a02f0-8c2b-4fd8-b1a8-e15841cca1d3
-# ╠═153cddc0-e6c7-4019-b31c-bd2b3d697a6e
+# ╟─153cddc0-e6c7-4019-b31c-bd2b3d697a6e
 # ╟─b14421cd-f505-47f2-80d7-e10cd20cdf47
