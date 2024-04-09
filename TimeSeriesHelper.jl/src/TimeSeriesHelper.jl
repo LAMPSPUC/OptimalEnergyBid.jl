26d1781fb6d65508916bf62d2bc41217aa608c35
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2024-04-09 18:35:46 +0200

    using

--- TimeSeriesHelper.jl/src/TimeSeriesHelper.jl
+++ TimeSeriesHelper.jl/src/TimeSeriesHelper.jl
@@ -4,6 +4,10 @@ using Distributions
 using HiddenMarkovModels
 using LinearAlgebra
 using Random
+using CSV
+using DataFrames
+
+Random = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
 
 include("structs.jl")
 include("read_table.jl")
